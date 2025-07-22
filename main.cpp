7d6666ada048a23df1090e1eb0f1aa223bf45b7a
yamakawatomoki1016 <115055022+yamakawatomoki1016@users.noreply.github.com>
2025-07-22 11:30:43 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <stdio.h>
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+	cout << "thread" << num << endl;
+}
 
 int main() {
+	thread t1(PrintThread, 1);
+	t1.join();  // thread1が終わるまで待つ
+
+	thread t2(PrintThread, 2);
+	t2.join();  // thread2が終わるまで待つ
+
+	thread t3(PrintThread, 3);
+	t3.join();  // thread3が終わるまで待つ
 
 	return 0;
 }
